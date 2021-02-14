fd99927f2c6b775320eadb1afc40d3cfa9ff9d89
Axarva <timsinaatharva@gmail.com>
2021-02-14 14:17:52 +0545

    Add newline

--- maxofarr.hs
+++ maxofarr.hs
@@ -12,4 +12,4 @@ quicksort [] = []
 quicksort (x:xs) =   
         let smallerSorted = quicksort [a | a <- xs, a <= x]  
             biggerSorted = quicksort [a | a <- xs, a > x]  
-        in  smallerSorted ++ [x] ++ biggerSorted  
+        in  smallerSorted ++ [x] ++ biggerSorted
