ea84d8553d601a4721016565a71a20c25b97a453
Axarva <timsinaatharva@gmail.com>
2021-02-16 13:44:09 +0545

    New bettermax

--- maxofarr.hs
+++ maxofarr.hs
@@ -3,13 +3,17 @@ maxOfArr [x] = x
 maxOfArr arr = if x > y
                  then x
                else maxOfArr $ tail $ quicksort arr
-               where 
+               where
                 x = head $ quicksort arr
                 y = head $ tail $ quicksort arr
 
-quicksort :: (Ord a) => [a] -> [a]  
-quicksort [] = []  
-quicksort (x:xs) =   
-        let smallerSorted = quicksort [a | a <- xs, a <= x]  
-            biggerSorted = quicksort [a | a <- xs, a > x]  
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+        let smallerSorted = quicksort [a | a <- xs, a <= x]
+            biggerSorted = quicksort [a | a <- xs, a > x]
         in  smallerSorted ++ [x] ++ biggerSorted
+
+-- I realized today I'm stupid, here's the better function
+betterMax :: (Foldable t, Ord a) => t a -> a
+betterMax = maximum
